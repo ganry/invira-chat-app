92cca8f9caa81a591009a8f35a75f2da44606b48
Henry <henry.keller@outlook.com>
2015-10-27 13:01:45 +0100

    added loading indicator to buttons

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -8,6 +8,14 @@ angular.module('inviraChatApp')
         };
 
         authCtrl.showNotification = function(message) {
+            var oldNotification = $('.ns-box');
+
+            if (oldNotification.length > 0)
+                oldNotification.hide(200, function() {
+                    $(this).remove();
+                });
+
+
             // create the notification
             var notification = new window.NotificationFx({
                 message : '<p>'+message+'</p>',
@@ -24,21 +32,44 @@ angular.module('inviraChatApp')
         };
 
         authCtrl.login = function() {
+            authCtrl.loading = true;
             Auth.$authWithPassword(authCtrl.user).then(function(auth) {
                 $state.go('home');
             }, function(error) {
+                authCtrl.loading = false;
                 authCtrl.error = error;
                 authCtrl.showNotification(error.message);
             });
         };
 
         authCtrl.register = function() {
+            authCtrl.loading = true;
             Auth.$createUser(authCtrl.user).then(function(auth) {
                 authCtrl.login();
             }, function(error) {
+                authCtrl.loading = false;
                 authCtrl.error = error;
                 authCtrl.showNotification(error.message);
             });
         };
 
-    });
+    })
+    .directive('laddaLoading', [
+        function() {
+            return {
+                link: function(scope, element, attrs) {
+                    var Ladda = window.Ladda;
+                    var ladda = Ladda.create(element[0]);
+                    // Watching login.loading for change
+                    scope.$watch(attrs.laddaLoading, function(newVal, oldVal) {
+                        // Based on the value start and stop the indicator
+                        if (newVal) {
+                            ladda.start();
+                        } else {
+                            ladda.stop();
+                        }
+                    });
+                }
+            };
+        }
+    ]);
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,6 +27,8 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-md5/angular-md5.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/spin.js/spin.js',
+      'bower_components/ladda/dist/ladda.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
